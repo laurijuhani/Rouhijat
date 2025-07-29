5b8d5107c75c8be048ac5400fda91cce83a772e3
laurijuhani <lauri.wilppu@gmail.com>
2025-07-29 15:30:47 +0300

    logged in users can now add content to history page

--- Frontend/app/(home)/history/page.tsx
+++ Frontend/app/(home)/history/page.tsx
@@ -1,101 +1,42 @@
-import Image from "next/image";
-import React from "react";
+import RenderPost from "@/components/tiptap/RenderPost";
 import { Timeline } from "@/components/ui/timeline";
+import { BlogPost } from "@/types/database_types";
 
-export default function History() {
-  const data = [
-    {
-      title: "Helmikuu 2025",
-      content: (
-        <div>
-          <p className="text-xs md:text-sm font-normal mb-8">
-            Paitojen suunnittelu alkoi. 
-          </p>
-          <p className="text-xs md:text-sm font-normal mb-8">
-            Paitoja alettiin suunnitelemaan Sepen ja Leksan toimesta. Suunnittelu oli yksi työmaa, jota ei auttanut vaativa johtoryhmä ja aivan paska 
-            softa, jolla suunnittelu tehtiin.
-          </p>
-          <div className="grid grid-cols-2 gap-4">
-            <Image
-              src="/history/paita_etu.png"
-              alt="startup template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover w-full "
-            />
-            <Image
-              src="/history/paita_taka.png"
-              alt="startup template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover w-full"
-            />
-            <p className="text-xs md:text-sm font-normal">
-              Havainnollistavat kuvat paidoista.
-            </p>
-       
-          </div>
-        </div>
-      ),
-    },
-    {
-      title: "Tammikuu 2025",
-      content: (
-        <div>
-          <p className="text-xs md:text-sm font-normal mb-8">
-            Ensimmäiset versiot Rouhijoiden logosta ja väreistä näkivät päivänvalon.
-          </p>
-          <p className="text-xs md:text-sm font-normal mb-8">
-            Ne eivät ole mitään taidetta, mutta onpa sentään jotain.
-          </p>
-          <div className="grid grid-cols-2 gap-4">
-            <Image
-              src="/logo.svg"
-              alt="Logo"
-              width={200}
-              height={200}
-              className="rounded-lg object-cover  w-full"
-            />
-            <Image
-              src="/logo.svg"
-              alt="feature template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover w-full"
-            />
-            <Image
-              src="/history/shredders.svg"
-              alt="bento template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover w-full"
-            />
-            <p className="text-neutral-200 text-xs md:text-sm font-normal mb-8 col-span-2">Ensimmäinen versio logosta. Tämä on jotain aivan järkyttävää :D</p>
-          </div>
-        </div>
-      ),
-    },
-    {
-      title: "Joulukuu 2024",
-      content: (
-        <div>
-          <p className="text-xs md:text-sm font-normal mb-4">
-            Idea Rouhijoiden perustamisesta syntyi Raisiossa sijaitsevassa kodassa.
-          </p>
-          <div className="mb-8">
-            <div className="flex gap-2 items-center text-xs md:text-sm">
-              Tällöin perustajat päättivät, että heidän on aika tehdä jotain suurempaa.
-              Ensimmäisiä kiinnityksiä joukkueeseen alettin jo tekemään ja juttu lensi.
-            </div>
-          </div>
-          
-        </div>
-      ),
-    },
-  ];
+const fetchPosts = async () => {
+  try {    
+    const res = await fetch(`${process.env.PRIVATE_BACKEND_URL}/history-posts`, {
+      next: { revalidate: parseInt(process.env.NEXT_PUBLIC_REVALIDATE || '600') }, // Use revalidate for successful fetch
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+    const blogs = await res.json() as BlogPost[];
+    return blogs.map((post) => ({
+      title: post.title,
+      content: <RenderPost content={post.content} />,
+    }));
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return [];  
+  }
+};
+
+
+
+
+const Page = async () => {
+  const data: { title: string, content: React.JSX.Element}[] = await fetchPosts();  
+
+  if (!data || data.length === 0) {
+    return <div>No posts available</div>;
+  }
+  
   return (
-    <div className="w-full">
+    <div>
       <Timeline data={data} />
     </div>
   );
-}
+};
+
+
+export default Page;
